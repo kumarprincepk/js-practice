39ad76ea764dbefc87e2f5453d8e800cced0d44b
Prince Kumar <63443684+kumarprincepk@users.noreply.github.com>
2024-11-02 18:38:35 +0000

    conversion operation revise

---
+++ basic/conversionOperation.js
@@ -0,0 +1,38 @@
+let score = "33abc";
+// let score = 33;
+// let score = null;
+// let score = undefined;
+// let score = true;
+// let score = "Prince";
+// let score = "";
+
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = 1;
+// let isLoggedIn = 0;
+// let isLoggedIn = "";
+// let isLoggedIn = "Prince";
+
+let boolenIsLoggedIn = Boolean(isLoggedIn);
+console.log(boolenIsLoggedIn)
+
+// 1 => true; 0=> false
+// "" => false
+// "Prince"=> true
+
+let someNumber = 33;
+
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
