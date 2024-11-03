c3e2f76a7700f1009556640169f56931a76cfbe3
Prince Kumar <63443684+kumarprincepk@users.noreply.github.com>
2024-11-03 17:31:27 +0000

    data type summery

---
+++ basic/dataTypeSummery.js
@@ -0,0 +1,46 @@
+
+//  Primitive
+
+//  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;      // undefined
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+// const bigNumber = 3456543576654356754n
+
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name: "hitesh",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof anotherId);
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+
+// +++++++++++
+
+// stack (Primitive) => it gets copy of the variable 
+
+// Heap (Non-Primitive) => it get refrence of original value if we change then it changes the original value also
